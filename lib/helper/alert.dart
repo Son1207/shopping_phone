53658a23c10735654caab4e1b0840d691bccf97b
Son1207 <94909220+Son1207@users.noreply.github.com>
2023-06-13 15:49:42 +0700

    update helper

---
+++ lib/helper/alert.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+AlertDialog alertLoading = AlertDialog(
+  content: Row(
+    children: [
+      const CircularProgressIndicator(),
+      Container(
+        margin: const EdgeInsets.only(left: 15),
+        child: const Text('Loading ...'),
+      )
+    ],
+  ),
+);
+
+AlertDialog alertSuccess = AlertDialog(
+  content: Row(
+    children: [
+      const Icon(
+        Icons.check,
+        size: 45,
+        color: Colors.green,
+      ),
+      Container(
+        margin: const EdgeInsets.only(left: 15),
+        child: const Text('Mua hàng thành công .'),
+      )
+    ],
+  ),
+);
